1e4ffded6155ff464438c9a31aafee8532cf57ca
Haekal <haekal762013@gmail.com>
2020-07-02 14:25:18 +0700

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout/main')
+
+@section('title', 'Haikal Mahendra')
+
+@section('container')
+
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-3">Hello, Haekal</h1>
+        </div>
+    </div>
+</div>
+@endsection
